8c3cf06a1948e307704a44d61a9c926e4adfe8b7
yevhenii.ashurov <yevhenii.ashurov@occe>
2024-01-11 22:09:01 +0100

    hw-6

--- homeWork/hw_7.sql
+++ homeWork/hw_7.sql
@@ -1,14 +1,14 @@
 -- Найти мин/стоимость товаров для каждой категории
 SELECT c.CategoryName, MIN(Price) MinProductPrice
 FROM products p
-         JOIN categories c ON p.CategoryID = c.CategoryID
+JOIN categories c ON p.CategoryID = c.CategoryID
 GROUP BY p.CategoryID;
 
 
 -- Вывести названия категорий, в которых более 10 товаров
 SELECT c.CategoryName
 FROM products p
-         JOIN categories c ON p.CategoryID = c.CategoryID
+JOIN categories c ON p.CategoryID = c.CategoryID
 GROUP BY c.CategoryID
 HAVING COUNT(*) > 10;
 
@@ -16,8 +16,8 @@ HAVING COUNT(*) > 10;
 -- Вывести три самых дешевых заказа
 SELECT o.*
 FROM orders o
-     JOIN order_details od ON od.OrderID = o.OrderID
-     JOIN products p on p.ProductID = od.ProductID
+JOIN order_details od ON od.OrderID = o.OrderID
+JOIN products p on p.ProductID = od.ProductID
 GROUP BY OrderID
 ORDER BY SUM(od.Quantity * p.Price)
 LIMIT 3;
